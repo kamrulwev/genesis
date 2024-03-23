b3b6b17eae6558f1f35d883e2a591e7c7c84cbb1
kamrulwev <155507235+kamrulwev@users.noreply.github.com>
2024-03-23 08:15:15 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide");
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.classList.add("active");
+      } else {
+        slide.classList.remove("active");
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+  }
+
+  document.querySelector('.next').addEventListener('click', nextSlide);
+  document.querySelector('.prev').addEventListener('click', prevSlide);
+
+  setInterval(nextSlide, 5500); // Change slide every 3 seconds
+});
+
+
+//
+
+//button open in new tab JS
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
